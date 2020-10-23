4e63bc92ce8cfa0f8c3af119328062fee939bc05
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-10-23 11:22:25 -0300

    footer updated

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,8 +9,8 @@ const Footer: React.FC = () => {
       <div className='footer'>
         <Button className='footer-item btn-text' href={`${Config.etherscan}${Config.Pool1.address}`} target='_blank' >Contract</Button>
         <Button className='footer-item btn-text' href='https://uniswap.info/pair/0xab2d2f5bc36620a57ec4bb60d6a7df2a847deab5' target='_blank' >Uniswap</Button>
-        <Button className='footer-item btn-text' href='#' target='_blank' >Twitter</Button>
-        <Button className='footer-item btn-text' href='#' target='_blank' >Telegram</Button>
+        <Button className='footer-item btn-text' href='https://twitter.com/UnificationUND' target='_blank' >Twitter</Button>
+        <Button className='footer-item btn-text' href='https://t.me/UnificationOfficial' target='_blank' >Telegram</Button>
       </div>
     </Container>
   )
