cf768198bbd0c4b0bfb7de3ef3c036fae03e4af9
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-10-26 14:38:43 -0300

    included build

---
+++ build/precache-manifest.294072a1b77ef9c8114a6a3315d74f1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7775df737e05401ed3a040150066ad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10373b08fea4599bd545",
+    "url": "/static/css/main.005929a4.chunk.css"
+  },
+  {
+    "revision": "96c4ecf39b4f61662ece",
+    "url": "/static/js/2.44c1c463.chunk.js"
+  },
+  {
+    "revision": "dcff997f9366a119d7ee653c31e5766a",
+    "url": "/static/js/2.44c1c463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10373b08fea4599bd545",
+    "url": "/static/js/main.54e23f24.chunk.js"
+  },
+  {
+    "revision": "dc8796411a4b380f77947626db152403",
+    "url": "/static/js/main.54e23f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d51a10a25bfcbf48b26",
+    "url": "/static/js/runtime-main.2a77b01c.js"
+  },
+  {
+    "revision": "f0168fa87e41fe513a90cf470715eff5",
+    "url": "/static/media/logo.f0168fa8.png"
+  }
+]);
